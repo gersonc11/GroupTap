77668f62620458e6baa0d2e51184d051dd48842b
William Turner <houndman42@gmail.com>
2019-07-18 14:59:32 -0400

    n/a

--- app/src/main/java/com/example/grouptap/MainActivity.java
+++ app/src/main/java/com/example/grouptap/MainActivity.java
@@ -2,11 +2,22 @@ package com.example.grouptap;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+    TextView textViewFragment;
+    Button buttonFragment;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         buttonFragment = setContentTransitionManager(R.id.button);
+         textViewFragment = setContentView(R.id.textView);
+
+
     }
 }
